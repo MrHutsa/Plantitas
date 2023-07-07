53ffb0be0d76298bb3f9d7992429e7fabd510244
MrHutsa <129012843+MrHutsa@users.noreply.github.com>
2023-07-07 07:49:39 -0400

    entrega final

--- venta/templates/venta/World-of-Plants-main/venta/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class VentaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'venta'
-    # add this
-    def ready(self):
-        import venta.signals  # noqa
